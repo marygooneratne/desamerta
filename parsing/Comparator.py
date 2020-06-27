06c2ede4808a65aaf5d7b6838ac2caf6fc764524
Mary Gooneratne <mmg53@duke.edu>
2020-06-27 17:02:52 -0500

    adding comparators

--- parsing/Comparator.py
+++ parsing/Comparator.py
@@ -1,6 +1,10 @@
+from abc import ABC, abstractmethod
+
 class Comparator:
     def __init__(self, value1, value2):
         self.value1 = value1
         self.value2 = value2
     
-    
+    @abstractmethod
+    def is_true(self):
+        pass
